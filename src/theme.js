ad9048e54b50b196e8cd22566fe4cb030a4893a6
Gerardo Solis <gerardoa.solis@yahoo.com>
2021-09-24 15:47:00 -0700

    Chore: Configure color palette for themes, including background, text, and gradients

--- src/theme.js
+++ src/theme.js
@@ -3,13 +3,32 @@ import { createTheme } from "@mui/material/styles";
 
 // A custom theme for this app
 const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 983,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
   palette: {
     primary: {
-      main: "#556cd6",
+      main: "rgba(255, 255, 255, 1)",
+      mainGradient: "linear-gradient(to  top right, black 50%, white )",
+      sencondaryGradient:
+        "linear-gradient(to  bottom, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0))",
     },
     secondary: {
-      main: "#619ec7",
+      main: "rgba(0, 0, 0, 1)",
     },
+    productLink: {
+      main: "rgba(246, 89, 40, 1)",
+    },
+    btnPrimary: {
+      main: "#2885F6",
+    },
+
     error: {
       main: red.A400,
     },
