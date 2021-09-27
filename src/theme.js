6f653aeede8fd9f3a38a3fffbc77945f00e50301
Gerardo Solis <gerardoa.solis@yahoo.com>
2021-09-27 14:53:13 -0700

    Chore: add typography for subtitle texts

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,6 @@
 import { red } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
+import { fontWeight } from "@mui/system";
 
 // A custom theme for this app
 const theme = createTheme({
@@ -15,9 +16,11 @@ const theme = createTheme({
   palette: {
     primary: {
       main: "rgba(255, 255, 255, 1)",
-      mainGradient: "linear-gradient(to  top right, black 45%, white )",
+      mainGradient:
+        "linear-gradient(90deg, rgba(0,0,0) 20%, rgba(0,0,0,0) 100%)",
+      // "linear-gradient(to  top right, black 45%, white )",
       sencondaryGradient:
-        "linear-gradient(to  bottom, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0))",
+        "linear-gradient(180deg, rgba(0,0,0,.65) 0%, rgba(0,0,0,0) 100%)",
     },
     secondary: {
       main: "rgba(0, 0, 0, 1)",
@@ -31,6 +34,19 @@ const theme = createTheme({
 
     error: {
       main: red.A400,
+    },
+  },
+  typography: {
+    subtitle1: {
+      fontSize: 18,
+      fontWeight: 700,
+    },
+    subtitle2: {
+      fontSize: 56,
+    },
+    subtitle3: {
+      fontSize: 18,
+      fontWeight: 400,
     },
   },
 });
