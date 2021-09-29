aaa95e1bdc2438873e64e0ee0bb59f5339cc95b9
Gerardo Solis <gerardoa.solis@yahoo.com>
2021-09-29 13:43:37 -0700

    Chore: add product content from cms with images using plug-ins

--- src/components/productContent/productBody.js
+++ src/components/productContent/productBody.js
@@ -1,60 +1,77 @@
 import * as React from "react";
 import { Typography } from "@mui/material";
 import ProductContainer from "./productContatiner";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Box from "@mui/material/Box";
 
-const ProductBody = ({ isMatch }) => {
+const ProductBody = ({ isMatch, data }) => {
+  const displayProductImages = (product) => {
+    const image = getImage(product.productImg);
+    return image;
+  };
+
   return (
     <section style={{ marginTop: "40px" }}>
       <ProductContainer>
-        <Box
-          sx={{
-            width: "370px",
-            mr: !isMatch ? "26px" : 0,
-            height: "240px",
-            bgcolor: "productBox.main",
-            mt: !isMatch ? "72px" : "30px",
-          }}
-        >
-          <div
-            style={{
-              marginLeft: "32px",
-              marginTop: !isMatch ? "112px" : "90px",
+        {data.map((product) => (
+          <Box
+            key={product.productImg.id}
+            sx={{
+              width: "385px",
+              mr: !isMatch ? "26px" : 0,
+              height: "240px",
+              bgcolor: "productBox.main",
+              mt: !isMatch ? "72px" : "30px",
+              position: "relative",
             }}
           >
-            <Typography variant={"subtitle1"} sx={{ color: "black" }}>
-              Augmented Reality
-            </Typography>
-            <Typography variant={"body2"} sx={{ color: "black", mt: "16px" }}>
-              An interactive experience like no other.
-            </Typography>
-            <Typography variant={"h6"} sx={{ mt: "24px" }}>
-              Learn more
-            </Typography>
-          </div>
-        </Box>
-        <Box
-          sx={{
-            width: "396px",
-            mr: "26px",
-            height: "240px",
-            bgcolor: "productBox.main",
-            mt: "72px",
-          }}
-        >
-          Hello World
-        </Box>
-        <Box
-          sx={{
-            width: "370px",
-            mr: "26px",
-            height: "240px",
-            bgcolor: "productBox.main",
-            mt: "72px",
-          }}
-        >
-          Hello World
-        </Box>
+            <div
+              style={{
+                marginLeft: "32px",
+                marginTop: !isMatch ? "112px" : "90px",
+              }}
+            >
+              <Typography variant={"subtitle1"} sx={{ color: "black" }}>
+                {product.product1}
+              </Typography>
+              <Typography variant={"body2"} sx={{ color: "black", mt: "16px" }}>
+                {product.product1description}
+              </Typography>
+              <Typography
+                variant={"h6"}
+                sx={{
+                  mt: "24px",
+                  "&:hover": {
+                    cursor: "pointer",
+                    color: "red",
+                  },
+                }}
+              >
+                {product.product1link}
+              </Typography>
+            </div>
+            <Box
+              sx={{
+                width: !isMatch ? "178px" : "160px",
+                height: !isMatch ? "220px" : "190px",
+                position: "absolute",
+                right: !isMatch ? "-20px" : "-35px",
+                bottom: !isMatch ? "90px" : "150px",
+              }}
+            >
+              <GatsbyImage
+                image={displayProductImages(product)}
+                alt={product.productImg.title}
+                style={{
+                  objectFit: "cover",
+                  position: "relative",
+                  height: "100%",
+                  width: "100%",
+                }}
+              />
+            </Box>
+          </Box>
+        ))}
       </ProductContainer>
     </section>
   );
