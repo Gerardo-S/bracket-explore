1f239f5c643d1a9bed8e57e9985dcb934487bd1c
Gerardo Solis <gerardoa.solis@yahoo.com>
2021-09-30 13:25:57 -0700

    Chore: rename image for og

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import NavBar from "./navBar/navBar";
 import { Helmet } from "react-helmet";
-import OgImg from "../images/thumbnail.png";
+import OgImg from "../images/homepage.png";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
